1aeb271569be16837bd104953e5eed508f8a0b4a
Swapna N A <swapnana86@gmail.com>
2022-10-27 17:10:44 +0530

    Test cases added

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,4 @@ from .views import AboutPageView, HomePageView
 urlpatterns = [
     path('about/', AboutPageView.as_view(), name='about'),
     path('',  HomePageView.as_view(), name='home'),
-    
 ]
